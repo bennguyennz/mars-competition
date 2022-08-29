db808df58313d810895f5e06ea68bb44f00129f7
Binh Nguyen <nguyenbinhit@gmail.com>
2022-08-29 18:38:18 +1200

    Initial commit

---
+++ MarsFramework/Competition/Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competition.Tests
+{
+    internal class Tests : Global.Base
+    {
+        [Test]
+        public void WhenILoginTheWebsite()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+    }
+}
